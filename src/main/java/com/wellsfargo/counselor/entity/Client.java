65198f96761600e0841092c12e3153081e34ec4a
fabioonn <147887552+fabioonn@users.noreply.github.com>
2024-05-24 10:25:43 -0700

    init

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,67 @@
+package com.wellsfargo.counselor.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientID;
+
+    private String name;
+    private String email;
+    private String phone;
+
+    @ManyToOne
+    private FinancialAdvisor advisor;
+
+    public Client() {}
+
+    public Client(String name, String email, String phone, FinancialAdvisor advisor) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+    }
+
+    // Getters and Setters
+    public Long getClientID() {
+        return clientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public FinancialAdvisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(FinancialAdvisor advisor) {
+        this.advisor = advisor;
+    }
+}
